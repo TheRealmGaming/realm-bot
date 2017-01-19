48839dc8a94bea8d04c8ddf204842fda364daa9e
Unknown <tdwells90@gmail.com>
2017-01-19 16:10:15 +0000

    Bot checking etc

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Import discord.js
-var Discord = require( 'discord.js' );
+var Discord = require('discord.js');
 
 // import axios
-var axios = require( 'axios' );
+var axios = require('axios');
 
 // Create client instance
 const bot = new Discord.Client();
@@ -16,59 +16,85 @@ const corpID = '98437545';
 // Industries ID
 const indyID = '98453454';
 
+// The global prefix
+const prefix = "-";
+
+// log our bot in
+bot.login( token );
+
+// On ready!
 bot.on( 'ready', function( event ) {
     console.log( 'Logged in' );
 });
 
 bot.on( 'message', message => {
-    // If message is '-created'
-    if( message.content === "--help" ) {
-        // Send response
-        message.reply( 'I could help you but that wouldn\'t be any fun!' )
-        .then( msg => console.log( 'Sent a reply to a member' ) )
-        .catch( console.error );
-    }
+
+    // If does not start with prefix
+    if ( !message.content.startsWith( prefix ) ) return;
+
+    // And if is a bot
+    if ( message.author.bot ) return;
 
     // Show latest EVE kill
-    if( message.content === "-last kill gaming" ) {
+    if (message.content === "-last kill gaming") {
         const zkillLink = 'https://zkillboard.com/api/kills/corporationID/' + corpID + '/limit/1/';
         axios.get( zkillLink )
-        .then( function( response ) {
-            const killID = response.data[0].killID;
-            message.channel.sendMessage( 'Latest The Realm Gaming kill: ' + 'https://zkillboard.com/kill/' + killID + '/' );
-        });
+            .then(function( response ) {
+                const killID = response.data[0].killID;
+                message.channel.sendMessage( 'Latest The Realm Gaming kill: ' + 'https://zkillboard.com/kill/' + killID + '/' );
+            });
     }
 
     // Show latest EVE death
-    if( message.content === "-last death gaming" ) {
+    if ( message.content === "-last death gaming" ) {
         const zkillLink = 'https://zkillboard.com/api/losses/corporationID/' + corpID + '/limit/1/';
         axios.get( zkillLink )
-        .then( function( response ) {
-            const killID = response.data[0].killID;
-            message.channel.sendMessage( 'Latest The Realm Gaming death: ' + 'https://zkillboard.com/kill/' + killID + '/' );
-        });
+            .then( function(response ) {
+                const killID = response.data[0].killID;
+                message.channel.sendMessage( 'Latest The Realm Gaming death: ' + 'https://zkillboard.com/kill/' + killID + '/' );
+            });
     }
 
     // Show latest Industries EVE kill
-    if( message.content === "-last kill indy" ) {
+    if ( message.content === "-last kill indy" ) {
         const zkillLink = 'https://zkillboard.com/api/kills/corporationID/' + indyID + '/limit/1/';
         axios.get( zkillLink )
-        .then( function( response ) {
-            const killID = response.data[0].killID;
-            message.channel.sendMessage( 'Latest The Realm Industries kill: ' + 'https://zkillboard.com/kill/' + killID + '/' );
-        });
+            .then( function( response ) {
+                const killID = response.data[0].killID;
+                message.channel.sendMessage( 'Latest The Realm Industries kill: ' + 'https://zkillboard.com/kill/' + killID + '/' );
+            });
     }
 
     // Show latest Industries EVE death
-    if( message.content === "-last death indy" ) {
+    if ( message.content === "-last death indy" ) {
         const zkillLink = 'https://zkillboard.com/api/losses/corporationID/' + indyID + '/limit/1/';
         axios.get( zkillLink )
-        .then( function( response ) {
-            const killID = response.data[0].killID;
-            message.channel.sendMessage( 'Latest The Realm Industries death: ' + 'https://zkillboard.com/kill/' + killID + '/' );
-        });
+            .then( function( response ) {
+                const killID = response.data[0].killID;
+                message.channel.sendMessage( 'Latest The Realm Industries death: ' + 'https://zkillboard.com/kill/' + killID + '/' );
+            });
     }
 });
 
-// log our bot in
-bot.login( token );
+// Welcoming new users!
+const newUsers = [];
+
+bot.on("guildMemberAdd", (member) => {
+    const guild = member.guild;
+    if ( !newUsers[guild.id] ) {
+        newUsers[guild.id] = new Discord.Collection();
+    }
+    newUsers[guild.id].set( member.user.id, member.user );
+
+    if ( newUsers[guild.id].size > 10 ) {
+        var userlist = newUsers[guild.id].map( u => u.mention() ).join( " " );
+        guild.channels.get( guild.id ).sendMessage( "Welcome our new users!\n ``` " + userlist + "```" );
+        newUsers[guild.id] = new Discord.Collection();
+    }
+
+    bot.on( "guildMemberRemove", ( member ) => {
+        const guild = member.guild;
+        if ( newUsers[guild.id].exists( "id", member.user.id ) ) newUsers.delete( member.user.id );
+    });
+
+});
